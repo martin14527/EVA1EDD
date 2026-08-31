805b9bb80fe8988bc5d2d625f0a133bf677f8da6
martin14527 <guillermovallejoloya@gmail.com>
2026-08-31 16:46:26 -0600

    Add files via upload

---
+++ EVA1_1_SCOPE/src/eva1_1_scope/EVA1_1_SCOPE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_scope;
+
+/**
+ *
+ * @author anoni
+ */
+public class EVA1_1_SCOPE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x = 100;
+       for(int i = 0; i<10; i++){
+       System.out.println("i = "+ i);
+       
+       System.out.println("valor final de la i"+ i);
+         System.out.println("valor final de la x"+ i);
+     
+       
+       }
+    }
+    
+}
