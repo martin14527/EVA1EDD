a6dd06e2222805ad8c308ec950185c70eb9859ec
martin14527 <guillermovallejoloya@gmail.com>
2026-09-04 11:39:29 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stack;
+public class EVA1_2_STACK {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+        System.out.print("inicia main");
+    }
+    public static void A(){
+         System.out.print("inicia A");
+          System.out.print("termina B");
+      B();
+    
+    }
+    public static void B(){
+     System.out.print("inicia B");
+     System.out.print("termina B");
+    int i = 5;
+    double d = 5.5;
+    }
+    
+}
---
+++ EVA1_3_STACKOVERFLOW/src/eva1_3_stackoverflow/EVA1_3_STACKOVERFLOW.java
@@ -0,0 +1,21 @@
+
+package eva1_3_stackoverflow;
+
+public class EVA1_3_STACKOVERFLOW {
+
+    
+    public static void main(String[] args) {
+       A();
+       
+    }
+    public static void A(){
+    
+    B();
+    }
+    public static void B(){
+    int i = 5;
+    double d=5.5;
+    A();
+    }
+    
+}
