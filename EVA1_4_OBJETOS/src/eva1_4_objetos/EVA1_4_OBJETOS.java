39935778aa7ec0ce89d49237dc1089e327a16675
martin14527 <guillermovallejoloya@gmail.com>
2026-09-07 16:51:10 -0600

    Add files via upload

---
+++ EVA1_4_OBJETOS/src/eva1_4_objetos/EVA1_4_OBJETOS.java
@@ -0,0 +1,23 @@
+
+package eva1_4_objetos;
+//estudiar heap
+public class EVA1_4_OBJETOS {
+
+    
+    public static void main(String[] args) {
+       Prueba prueba = new  Prueba();
+       
+       System.out.println(prueba); //guarda la direccion, la direccion el stakc y el heap guarda el objeto
+       //lo que imprime es una referncia, o apuntadores
+       //Gargabe collector;
+       prueba  = null;
+    }
+    
+}
+class Prueba{}
+
+
+
+
+
+
